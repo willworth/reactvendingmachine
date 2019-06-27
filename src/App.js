b26ff1bbeb076ad62462f1636ce9eff0f95d7eb4
Will Worth <will.worth@gmail.com>
2019-06-27 13:19:49 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Chips from "./Chips";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import Navbar from "./Navbar";
+import VendingMachine from "./VendingMachine";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' render={() => <VendingMachine />} />
+          <Route exact path='/soda' render={() => <Soda />} />
+          <Route exact path='/sardines' render={() => <Sardines />} />
+          <Route exact path='/chips' render={() => <Chips />} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
